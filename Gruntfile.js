86ea544a0487ff6386b5e207f9bc1923cd4af8e6
Pawel Szymczykowski <makenai@gmail.com>
2015-03-30 23:11:46 -0700

    push to origin for bumps

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
     bump: {
       options: {
         files: ['package.json','bower.json'],
-        pushTo: 'master'
+        pushTo: 'origin'
       }
     }
   });
